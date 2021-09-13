623bf17b473196ead83a4fb7f2a0be73de0e202a
AdnannM <48089787+AdnannM@users.noreply.github.com>
2021-09-13 09:42:32 +0200

    Preloading a SQLite Database Using CoreData

--- CoreDataPreload/Controller/MenuTableViewController.swift
+++ CoreDataPreload/Controller/MenuTableViewController.swift
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class MenuTableViewController: UITableViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
---
+++ CoreDataPreload/CustomCell/MenuTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  MenuTableViewCell.swift
+//  CoreDataPreload
+//
+//  Created by Adnann Muratovic on 13.09.21.
+//
+
+import UIKit
+
+class MenuTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
